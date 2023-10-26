c46304e69e634831111294aab54acbd7ad02096e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:03:13 +0200

    test of SQL export

--- EOGeoDataLayer/schema.sql
+++ EOGeoDataLayer/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model EOGeoDataLayer of the subject dataModel.SatelliteImagery for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE EOGeoDataLayer_type AS ENUM ('EOGeoDataLayer');
-CREATE TABLE EOGeoDataLayer (address JSON, alternateName TEXT, areaServed TEXT, contentInformation JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, geoMetadata TEXT, isOutputOf TEXT, localServerPath TEXT, name TEXT, owner JSON, source TEXT, storageFormat TEXT, type EOGeoDataLayer_type);
+CREATE TABLE EOGeoDataLayer (address JSON, alternateName TEXT, areaServed TEXT, contentInformation JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, geoMetadata TEXT, id TEXT PRIMARY KEY, isOutputOf TEXT, localServerPath TEXT, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, storageFormat TEXT, type EOGeoDataLayer_type);
