5ff468a7fc79906585e832c12ab8600805b783b4
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:26:46 +0200

    created example of code

--- EOInstrument/code/code_for_using_dataModel.SatelliteImagery_EOInstrument.py
+++ EOInstrument/code/code_for_using_dataModel.SatelliteImagery_EOInstrument.py
@@ -24,33 +24,38 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "EOInstrument"
 subject = "dataModel.SatelliteImagery"
-carriedOn = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:EOSatellitePlatform:154'}"
+carriedOn = "urn:ngsi-ld:EOSatellitePlatform:154"
 attribute = "carriedOn"
 value = carriedOn
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-instrumentID = "{'type': 'Property', 'value': 'SAR-C'}"
+instrumentID = "MSI"
 attribute = "instrumentID"
 value = instrumentID
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-instrumentName = "{'type': 'Property', 'value': 'Synthetic Aperture Radar (C-band)'}"
+instrumentName = "Multi-Spectral Instrument"
 attribute = "instrumentName"
 value = instrumentName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-operationalMode = "{'type': 'Property', 'value': 'IW'}"
-attribute = "operationalMode"
-value = operationalMode
+swathID = "S2"
+attribute = "swathID"
+value = swathID
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
